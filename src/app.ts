dbbf58ab1f07aafec61ba8835629f29d8a428857
deepak123-m <deepak79935@gmail.com>
2025-08-10 23:18:49 +0530

    second commit

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,23 @@ import router from './routes/flashcardRoutes';
 
 dotenv.config();
 
+
+
+
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Database connection
 connectDB();
 
-// Routes
 app.use('/', router);
 
-// Error handling middleware
+
+
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something broke!' });
